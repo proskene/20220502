09bad7ccc6a4e3932f8d9d14f4d8e51eaaeb5c9b
LEEOU <leeousys@gmail.com>
2022-05-02 15:29:58 +0900

    444

--- leeou.cpp
+++ leeou.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main()
 {
-	for (int a = 1; a <= 5; a++)
+	int count = 0;
+	cin >> count;
+	for (int a = 1; a <= count; ++a)
 
 	{
-		for (int i = 1; i <= a; i++)
+		for (int i = 1; i <= a; ++i)
 		{
 			cout << "*";
 		}
-		cout << endl;
+		cout << '\n';
 	}
 	return 0;
 }
