c7a2daa0f4d4e6ab98c1c17f2b022d4a26574453
LEEOU <leeousys@gmail.com>
2022-05-02 09:39:44 +0900

    20220502

---
+++ 소스.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A;
+	A = 100;
+
+	cout << A + 50 << endl;
+	cout << A - 50 << endl;
+	cout << A * 50 << endl;
+	cout << A / 50 << endl;
+	cout << A % 3 << endl;
+
+	float B;
+	B = 10.5f;
+
+	cout << B + 50 << endl;
+	cout << B - 50 << endl;
+	cout << B * 50 << endl;
+	cout << B / 50 << endl;
+}
